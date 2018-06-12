f10fa9e7594580cad898f2b6c7affd612ec4c146
hsakhiya <hirenh.sakhiya@gmail.com>
2018-06-12 16:50:09 +0530

    Changed UI of Resource list and Resource Detail screen.Removed loading spinner when entering a new screen within the cache timeout.

--- src/screens/Resources/ResourceList/index.js
+++ src/screens/Resources/ResourceList/index.js
@@ -7,7 +7,8 @@ import {
     FlatList,
     View,
     ScrollView,
-    AsyncStorage
+    AsyncStorage,
+    ImageBackground
 } from 'react-native';
 
 import Styles from './styles';
@@ -16,6 +17,8 @@ import Footer from '@footer'
 import { Loader } from '@components';
 import { getResources,updateTimeInterval } from "@api";
 import moment from 'moment'
+import {Colors, Images, FontSizes} from '@theme';
+import { MediaQuery } from "react-native-responsive";
 
 export default class Resources extends Component {
     constructor(props) {
@@ -23,7 +26,7 @@ export default class Resources extends Component {
         Props=this.props;
         this.state = ({
             resourceIndexes: [],
-            loaderVisible: true
+            loaderVisible: false
         })
     }
 
@@ -42,6 +45,10 @@ export default class Resources extends Component {
 
                     if(difference >= updateTimeInterval)
                     {
+                        this.setState({
+                            loaderVisible: true
+                        })
+
                         await AsyncStorage.setItem('lastRefereshTimeResource', currrentTime);
                         const ds = await getResources()
 
@@ -66,12 +73,15 @@ export default class Resources extends Component {
 
                         this.setState({
                             resourceIndexes: resourceIndexes,
-                            loaderVisible: false
                         })
                     }   
                 }
                 else {
                   // do something else
+                    this.setState({
+                            loaderVisible: true
+                        })
+
                     var currrentTime = moment(new Date()).format("HH:mm:ss");
                     await AsyncStorage.setItem('lastRefereshTimeResource', currrentTime); 
                     const ds = await getResources()
@@ -96,34 +106,50 @@ export default class Resources extends Component {
     renderResourceItem({item, index}){
         const {navigate} = this.props.navigation
         return (
-            <TouchableOpacity style={Styles.item} onPress={()=>{navigate("ResourceDetail", {resourceIndex: index})}}>
-                <Text style={Styles.txttitle}>{item.title}</Text>
+
+             <TouchableOpacity style={Styles.item} onPress={()=>{navigate("ResourceDetail", {resourceIndex: index})}}>
+                            <View style={Styles.cardView}>
+                                <Text medium style={Styles.cardtitle}>{item.title}</Text>
+                                <Image source={Images.icon_left_arrow} resizeMode='contain' />
+                            </View>
             </TouchableOpacity>
         )
     }
 
     render() {
-        return (
-            
-            <View style={Styles.container}>
-                <View style={Styles.scrollcontainer}> 
+        return ( 
+
+            <ImageBackground source={Images.bg_navigation} resizeMode="stretch" style={Styles.container} >
+
+                <ScrollView contentContainerStyle={Styles.scroll}>
                     <Loader loading={this.state.loaderVisible}/>
-                    <ScrollView contentContainerStyle={Styles.scroll}>
-                        <Text bold style={Styles.title}>resources</Text>
-                        <Text style={Styles.subtitle}>
-                            View list of resources and use to learn more
+                    <View style={Styles.titleView}>
+                        <Text large style={Styles.title}>Resource library</Text>
+                        <Text medium style={Styles.subtitle}>
+                            Extra information and resources.
                         </Text>
-                 
+                    </View>
+
+                    <MediaQuery minDeviceWidth={768}>
                         <FlatList
                             numColumns = {2}
                             data = {this.state.resourceIndexes}
                             renderItem = {this.renderResourceItem.bind(this)}
                             keyExtractor={item => item.title}
                         />
-                    </ScrollView>
-                </View>
-                <Footer />
-            </View>    
+                    </MediaQuery>
+
+                    <MediaQuery maxDeviceWidth={767}>
+                        <FlatList
+                            numColumns = {1}
+                            data = {this.state.resourceIndexes}
+                            renderItem = {this.renderResourceItem.bind(this)}
+                            keyExtractor={item => item.title}
+                        />
+                    </MediaQuery>
+
+                </ScrollView>
+            </ImageBackground> 
         );
     }
 }
--- src/screens/Resources/ResourceList/styles.js
+++ src/screens/Resources/ResourceList/styles.js
@@ -5,57 +5,129 @@ import {
     Dimensions,
 } from 'react-native';
 
-import {Colors} from '@theme';
+import {Colors, MediaQueries} from '@theme';
 
 const { width,height } = Dimensions.get('window');
+import { responsiveWidth, responsiveHeight } from 'react-native-responsive-dimensions';
+import { MediaQueryStyleSheet } from "react-native-responsive";
 
 
-export default {
+export default MediaQueryStyleSheet.create({
 
     container: {
         flex: 1, 
         backgroundColor: Colors.backgroundPrimary,
     },
     scroll:{
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    scrollcontainer:{
-        marginBottom:height/15,
+        paddingHorizontal: width/50,
+        paddingVertical:height/60
     },
     title: {
-        fontSize: width/15,
-        marginTop: height/18,
-        color: Colors.textPrimary,
-        margin: 8,
+        color: Colors.Navy,
+        fontWeight:"200"
     },
-    txttitle: {
-        fontSize: width/30,
-        fontWeight: 'bold',
-        color: Colors.textPrimary,
+    cardtitle: {
+        fontWeight: '300',
+        color: Colors.Navy,
         margin: 8,
+        textAlign:'center'
+    },
+    cardView:{
+        flexDirection:'row',
+        alignItems:'center'
     },
     subtitle: {
-        fontSize: width/30,
-        color: Colors.textPrimary,
+        color: Colors.textSecondary,
         textAlign: 'center',
-        marginBottom: height/25,
-    },
-    buttomBar: {
-        flexDirection: 'row',
+        marginTop:2,
+        fontWeight:"100"
     },
     item: {
-        width: width/3,
-        height: width/8,
+        flex: 1,
         backgroundColor: Colors.backgroundSecondary,
-        borderRadius: 4,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
+        borderRadius: 8,
+        shadowColor: '#000',
+        shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
         shadowOpacity: 0.5,
-        shadowRadius: 2,
-        margin: 8,    
+        shadowRadius: 0,
+        margin: width/60,   
+        paddingVertical:height/80,  
+        alignItems:'center'
+    },
+    itemView:{
+    },
+    firstrowItem: {
+        flex: 1,
+        backgroundColor: Colors.backgroundSecondary,
+        borderRadius: responsiveWidth(1.2),
+        borderTopWidth: responsiveWidth(1.2),
+        borderTopColor: Colors.Red,
+        margin: width/60,   
+        paddingVertical:height/30,
+        shadowColor: '#000',
+        shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
+        shadowOpacity: 0.5,
+        shadowRadius: 0,
+    },
+    titleView: {
+        backgroundColor: Colors.backgroundSecondary,
+        borderTopColor: Colors.Navy,
+        borderRadius: responsiveWidth(1.2),
+        borderTopWidth: responsiveWidth(1.2),
+        shadowColor: '#000',
+        shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
+        shadowOpacity: 0.5,
+        shadowRadius: 0,
+        marginBottom: width/50,
+        marginHorizontal:width/60,   
+        paddingVertical:height/45,
+        paddingHorizontal:width/20,
         justifyContent: 'center',
         alignItems: 'center',
     },
-
-};
+    icon:{
+        width: width/6,
+        height: height/9,
+        resizeMode: 'contain', 
+        tintColor: Colors.Red,
+    }
+}, {
+    [MediaQueries.iPad] : {
+        scroll:{
+            paddingHorizontal: width/10,
+            paddingVertical:height/40
+        },
+        item: {
+            flex: 1,
+            backgroundColor: Colors.backgroundSecondary,
+            borderRadius: 8,
+            shadowColor: '#000',
+            shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
+            shadowOpacity: 0.5,
+            shadowRadius: 0,
+            margin: width/60,   
+            paddingVertical:height/80,  
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        itemView:{
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        firstrowItem: {
+            flex: 1,
+            backgroundColor: Colors.backgroundSecondary,
+            borderRadius: responsiveWidth(1.2),
+            borderTopWidth: responsiveWidth(1.2),
+            borderTopColor: Colors.Red,
+            margin: width/60,   
+            paddingVertical:height/30,
+            shadowColor: '#000',
+            shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
+            shadowOpacity: 0.5,
+            shadowRadius: 0,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }
+    }
+});
