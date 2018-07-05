19771610b9e1d529b3830ab186d49d1b01fc43ac
hsakhiya <hirenh.sakhiya@gmail.com>
2018-07-05 16:23:31 +0530

    Fixed cut off issue of go back button in looking after yourself screen in landscape mode.

--- src/screens/GetHelp/LookAfterYourself/styles.js
+++ src/screens/GetHelp/LookAfterYourself/styles.js
@@ -10,16 +10,18 @@ import {Colors, MediaQueries} from '@theme';
 const { width,height } = Dimensions.get('window');
 import { responsiveWidth, responsiveHeight } from 'react-native-responsive-dimensions';
 import { MediaQueryStyleSheet } from "react-native-responsive";
+import { deviceWidth, deviceHeight, windowHeight, windowWidth } from "@ResponsiveDimensions";
 
 
 export default MediaQueryStyleSheet.create({
 
     container: {
        flex: 1, 
-        backgroundColor: Colors.backgroundPrimary,
+    backgroundColor: Colors.backgroundPrimary,
     },
     scroll:{
-        paddingHorizontal: width/50,
+        
+        alignSelf: 'center',
     },
     title: {
         color: Colors.Navy,
@@ -31,8 +33,8 @@ export default MediaQueryStyleSheet.create({
         marginVertical:width/50,
     },
     middleimage: {
-        height:height/7.5,
-        width:width/4,
+        height:height/6,
+        width:width/4.5,
     },
     titleView: {
         backgroundColor: Colors.backgroundSecondary,
@@ -43,21 +45,20 @@ export default MediaQueryStyleSheet.create({
         shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
         shadowOpacity: 0.5,
         shadowRadius: 0,
-        marginBottom: width/50,
         marginTop : width/35, 
-        marginHorizontal:width/60,   
-        paddingVertical:height/45,
+        marginBottom: width/35,
+        paddingVertical:height/60,
         paddingHorizontal:width/20,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    buttonBackView:{
-        flexDirection:'row',
+    buttonBar: {
         backgroundColor:Colors.Sand,
-        height:height/16,
-        justifyContent:'space-between',
-        alignItems:'center',
-        paddingHorizontal:width/30
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: deviceWidth(0.5),
+        paddingHorizontal: deviceWidth(10),
+        alignItems: 'center',
     },
     buttonBack:{
         height:height/22,
@@ -72,60 +73,50 @@ export default MediaQueryStyleSheet.create({
         shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
         shadowOpacity: 0.5,
         shadowRadius: 0,
-        margin: 8,    
-        paddingHorizontal:width/15,
-        paddingVertical:width/30,
+        paddingHorizontal:width/25,
+        paddingVertical:width/50,
+        marginBottom : width/35, 
         justifyContent: 'center',
         alignItems: 'center',
     },
-
-}, {
+},
+{
     [MediaQueries.iPad] : {
+        scroll: {
+            paddingHorizontal: deviceWidth(10),
+        },
+        buttonBar: {
+            paddingHorizontal: deviceWidth(11),            
+        },
         titleView: {
-            backgroundColor: Colors.backgroundSecondary,
-            borderTopColor: Colors.Navy,
-            borderRadius: responsiveWidth(1.2),
-            borderTopWidth: responsiveWidth(1.2),
-            shadowColor: '#000',
-            shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
-            shadowOpacity: 0.5,
-            shadowRadius: 0,
-            marginBottom: width/50,
             marginTop : width/25, 
-            marginHorizontal:width/60,   
-            paddingVertical:height/45,
-            paddingHorizontal:width/20,
-            justifyContent: 'center',
-            alignItems: 'center',
         },
         middleimage: {
             height:height/6,
             width:width/4.5,
         },
-        scroll:{
-            paddingHorizontal: width/10,
+        itemView: {
+            paddingHorizontal:width/25,
+            paddingVertical:width/50,
         },
-        buttonBackView:{
-            flexDirection:'row',
-            backgroundColor:Colors.Sand,
-            height:height/16,
-            justifyContent:'space-between',
-            alignItems:'center',
-            paddingHorizontal:width/8
+    },
+    [MediaQueries.iPhone] : {
+        scroll: {
+            paddingHorizontal: deviceWidth(5),
+        },
+        buttonBar: {
+            paddingHorizontal: deviceWidth(6),            
+        },
+        titleView: {
+            marginTop : width/35, 
+        },
+        middleimage: {
+            height:height/7.5,
+            width:width/4,
         },
         itemView: {
-            flex:1,
-            backgroundColor: Colors.backgroundSecondary,
-            borderRadius: 8,
-            shadowColor: '#000',
-            shadowOffset: { width: responsiveWidth(1.2), height: responsiveWidth(1.2) },
-            shadowOpacity: 0.5,
-            shadowRadius: 0,
-            margin: 8,    
-            paddingHorizontal:width/25,
+            paddingHorizontal:width/20,
             paddingVertical:width/40,
-            justifyContent: 'center',
-            alignItems: 'center',
         },
     }
 });
