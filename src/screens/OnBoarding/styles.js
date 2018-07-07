31ae29390efb11bd261099c638e68c9ff410088c
hsakhiya <hirenh.sakhiya@gmail.com>
2018-07-07 17:46:30 +0530

    Changed UI of OnBoarding screen.Added one slide in Onboarding screen.Fixed issue of Onboarding slides when changing dimensions.

--- src/screens/OnBoarding/styles.js
+++ src/screens/OnBoarding/styles.js
@@ -10,14 +10,19 @@ import {
 } from 'react-native';
 
 //import {Colors} from '../../theme'; // use for theme color
-import {Colors} from '@theme'
+import {Colors, MediaQueries} from '@theme'
 const { width, height } = Dimensions.get('window'); // Detect screen width and height
 import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';// use for responsive screen UI
 import { deviceWidth, deviceHeight, windowHeight, windowWidth } from "@ResponsiveDimensions";
+import { MediaQueryStyleSheet } from "react-native-responsive";
 
-export default {
+export default MediaQueryStyleSheet.create({
   slide: {           
     alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: responsiveWidth(0.5), height: responsiveWidth(0.5) },
+    shadowOpacity: 0.5,
+    shadowRadius: 0, 
   },
   scrollcontainer:{
     alignItems:'center',
@@ -30,11 +35,21 @@ export default {
   },
   middleimage:{
     height:deviceHeight(33),
+    width:deviceWidth(70)
   },
   descText: {
     textAlign:'center',
-    color:Colors.textSecondary,
+    color:Colors.Navy,
     fontSize:deviceWidth(2.5)
+  },
+  titleText: {
+    textAlign:'center',
+    color:Colors.white,
+  },
+  subTitleText: {
+    textAlign:'center',
+    color:Colors.white,
+    marginTop:deviceWidth(1)
   },
   container: {
     flex: 1, 
@@ -48,8 +63,8 @@ export default {
   },
   dot: {
     backgroundColor: Colors.white,
-    width: deviceWidth(12),
-    height: 5,
+    width: deviceWidth(8),
+    height: deviceHeight(0.6),
     borderWidth:1,
     borderColor:Colors.Olive,
     marginLeft: 2,
@@ -62,26 +77,40 @@ export default {
     flexDirection:'row',
     justifyContent:'center',
     alignItems:'center',
-
   },
   buttonPrev: {         
-    width:deviceWidth(25),
-    height:deviceHeight(5.5),
+    width:deviceWidth(20),
+    paddingVertical:deviceHeight(1),
     alignItems:'center',
     justifyContent:'center',
     marginTop:deviceHeight(3)
   },
   buttonNext: {       
-    width:deviceWidth(25),
-    height:deviceHeight(5.5),
+    width:deviceWidth(20),
+    paddingVertical:deviceHeight(1),
+    borderWidth: 2,
+    borderColor : Colors.Navy,
     alignItems:'center',
     justifyContent:'center',
     marginTop:deviceHeight(3)
   },
+  buttonpageView: {
+    width:deviceWidth(70),
+    backgroundColor:Colors.white,
+    shadowColor: '#000',
+    shadowOffset: { width: responsiveWidth(0.5), height: responsiveWidth(0.5) },
+    shadowOpacity: 0.5,
+    shadowRadius: 0, 
+    marginBottom:deviceWidth(9)
+  },
   textView:{
     backgroundColor:Colors.white,
-    paddingHorizontal: deviceWidth(10),
-    paddingVertical:deviceHeight(2),
+    paddingHorizontal: deviceWidth(5),
+    paddingVertical:deviceHeight(1),
+    height:deviceWidth(15),
+    width:deviceWidth(70),
+    alignItems:'center',
+    justifyContent:'center'
   },
   background: {
     flex: 1,
@@ -90,4 +119,46 @@ export default {
     backgroundColor: 'transparent',
     alignItems: 'center',
   },
-};
+},
+{
+    [MediaQueries.iPad] : {
+        scrollcontainer:{
+          paddingVertical:deviceHeight(7),
+        },
+        subTitleView: {
+          marginBottom:deviceWidth(5)
+        },
+        subTitleText: {
+          marginTop:deviceWidth(1)
+        },
+        middleimage:{
+          width:deviceWidth(60)
+        },
+        textView:{
+          width:deviceWidth(60),
+        },
+        buttonpageView: {
+          width:deviceWidth(60),
+        },
+    },
+    [MediaQueries.iPhone] : {
+        scrollcontainer:{
+          paddingVertical:deviceHeight(10),
+        },
+        subTitleView: {
+          marginBottom:deviceWidth(9)
+        },
+        subTitleText: {
+          marginTop:deviceWidth(2)
+        },
+        middleimage:{
+          width:deviceWidth(70)
+        },
+        textView:{
+          width:deviceWidth(70),
+        },
+         buttonpageView: {
+          width:deviceWidth(70),
+        },
+    }
+});
