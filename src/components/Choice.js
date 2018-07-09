1b7aa618809ebb93d814464a514b80c7190827f9
Unknown <lovecoding530@gmail.com>
2018-07-09 01:49:43 -0700

    fixed select/remove button width, info alert

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -33,12 +33,12 @@ export default class Choice extends Component {
                     <View style={styles.container}>
                         <Icon name={'check'} color={Colors.Navy} style={styles.icon} size={20}/>
                         <Text smallMedium style={styles.text}>{this.props.text}</Text>            
-                        <Button small light bold color={Colors.Red} buttonStyles={{width: deviceHeight(8)}} onPress={()=>{this.props.onPress(this.props.index)}}>Remove</Button>
+                        <Button small light bold color={Colors.Red} buttonStyles={{width: deviceHeight(8), paddingHorizontal: 0}} onPress={()=>{this.props.onPress(this.props.index)}}>Remove</Button>
                     </View>
                     :
                     <View style={styles.container}>
                         <Text smallMedium style={styles.text}>{this.props.text}</Text>            
-                        <Button small light bold buttonStyles={{width: deviceHeight(8)}} onPress={()=>{this.props.onPress(this.props.index)}}>Select</Button>
+                        <Button small light bold buttonStyles={{width: deviceHeight(8), paddingHorizontal: 0}} onPress={()=>{this.props.onPress(this.props.index)}}>Select</Button>
                     </View>
             }
             </View>    
