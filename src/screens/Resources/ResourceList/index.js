2163ef8b3eab90d44b0e34e5452bb9e20dac04f0
Unknown <lovecoding530@gmail.com>
2018-09-24 20:03:43 -0700

    resource library

--- src/screens/Resources/ResourceList/index.js
+++ src/screens/Resources/ResourceList/index.js
@@ -14,11 +14,12 @@ import {
 import Styles from './styles';
 import Text from '@text'
 import Footer from '@footer'
-import { Loader } from '@components';
+import { Loader, Card } from '@components';
 import { getResources,updateTimeInterval } from "@api";
 import moment from 'moment'
 import {Colors, Images, FontSizes} from '@theme';
 import { MediaQuery } from "react-native-responsive";
+import { deviceWidth, deviceHeight, windowHeight, windowWidth } from "@ResponsiveDimensions";
 
 export default class Resources extends Component {
     constructor(props) {
@@ -31,75 +32,6 @@ export default class Resources extends Component {
     }
 
     async componentDidMount() {
-        // try
-        // {
-        //     let value = await AsyncStorage.getItem('lastRefereshTimeResource');
-
-        //     if (value != null){
-        //         // do something
-        //         var currrentTime = moment(new Date()).format("HH:mm:ss");
-        //         var startTime=moment(value, "HH:mm:ss");
-        //         var endTime=moment(currrentTime, "HH:mm:ss");
-        //         var duration = moment.duration(endTime.diff(startTime));
-        //         var difference = moment.utc(+duration).format('H');
-
-        //         if(difference >= updateTimeInterval)
-        //         {
-        //             this.setState({
-        //                 loaderVisible: true
-        //             })
-
-        //             await AsyncStorage.setItem('lastRefereshTimeResource', currrentTime);
-        //             const ds = await getResources()
-
-        //             var resourceIndexes = [];
-        //             for(var i = 0; i < ds.resources.length; i ++){
-        //                 resourceIndexes.push(ds.resources[i]);
-        //             }
-
-        //             this.setState({
-        //                 resourceIndexes: resourceIndexes,
-        //                 loaderVisible: false
-        //             })
-        //         }
-        //         else
-        //         {
-        //             const ds = await getResources(true)
-
-        //             var resourceIndexes = [];
-        //             for(var i = 0; i < ds.resources.length; i ++){
-        //                 resourceIndexes.push(ds.resources[i]);
-        //             }
-
-        //             this.setState({
-        //                 resourceIndexes: resourceIndexes,
-        //             })
-        //         }
-        //     }
-        //     else {
-        //         // do something else
-        //         this.setState({
-        //                 loaderVisible: true
-        //             })
-
-        //         var currrentTime = moment(new Date()).format("HH:mm:ss");
-        //         await AsyncStorage.setItem('lastRefereshTimeResource', currrentTime);
-        //         const ds = await getResources()
-
-        //         var resourceIndexes = [];
-        //         for(var i = 0; i < ds.resources.length; i ++){
-        //             resourceIndexes.push(ds.resources[i]);
-        //         }
-
-        //         this.setState({
-        //             resourceIndexes: resourceIndexes,
-        //             loaderVisible: false
-        //         })
-        //     }
-        // }
-        // catch (error) {
-        //     // Error retrieving data
-        // }
 
         var json = await getResources(true)
         if(!json){
@@ -121,30 +53,25 @@ export default class Resources extends Component {
     renderResourceItem({item, index}){
         const {navigate} = this.props.navigation
         return (
-
-                <TouchableOpacity style={Styles.item} onPress={()=>{navigate("ResourceDetail", {resourceIndex: index})}}>
-                            <View style={Styles.cardView}>
-                                <Text smallmedium bold style={Styles.cardtitle}>{item.title}</Text>
-                                <Image source={Images.icon_left_arrow} resizeMode='contain' />
-                            </View>
-                </TouchableOpacity>
-
+            <Card style={Styles.cardView} onPress={()=>{navigate("ResourceDetail", {resourceIndex: index})}}>
+                <Text smallmedium center bold light>{item.title}</Text>
+            </Card>
         )
     }
 
     render() {
         return (
 
-            <ImageBackground source={Images.bg_more_information} resizeMode="stretch" style={Styles.container} >
+            <View style={Styles.container} >
 
                 <ScrollView contentContainerStyle={Styles.scroll}>
                     <Loader loading={this.state.loaderVisible}/>
-                    <View style={Styles.titleView}>
-                        <Text large style={Styles.title}>Resource library</Text>
-                        <Text medium style={Styles.subtitle}>
+                    <Card topbar={{color: Colors.navy}} style={Styles.titleView}>
+                        <Text large center color={Colors.navy} style={Styles.title}>Resource library</Text>
+                        <Text medium center style={Styles.subtitle} color={Colors.navy}>
                             Extra information and resources.
                         </Text>
-                    </View>
+                    </Card>
 
                     <MediaQuery minDeviceWidth={768}>
                         <FlatList
@@ -166,7 +93,7 @@ export default class Resources extends Component {
                     </MediaQuery>
 
                 </ScrollView>
-            </ImageBackground>
+            </View>
         );
     }
 }
